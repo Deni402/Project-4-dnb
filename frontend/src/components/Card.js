50e8832a219e4f5eb883b9d5a501c830f053d152
Denisa <tdenisamihaela@yahoo.com>
2020-01-29 16:31:23 +0200

    update

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -2,30 +2,49 @@ import React from 'react'
 
 const Card = ({ largeimageurl, eventname, postcode, startdate, openingtimes, minage, address }) => {
   return (
-    <div className="column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
-      <div className="card">
-        <div className="card-image">
-          <figure className="image is-4by3">
-            <img src={largeimageurl} alt={eventname}/>
-          </figure>
-        </div>
-        <div className="cardName-content">
-          <div className="media">
-            <div className="media-left">
-          
-            </div>
-      
-          </div>
-  
-          <div className="content">
-            fghjkjhgfghjhg<a>@bulmaio</a>.
-            <a href="#">#css</a> <a href="#">#responsive</a>
-            <br/>
-            <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-          </div>
-        </div>
+
+    < div className="card" >
+      <div className="card-image">
+        <figure className="image is-2by3">
+          <img src={largeimageurl} alt={eventname}/>
+        </figure>
       </div>
-    </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="subtitle is-4 is-size-5 has-text-white">{address}</p>
+            <p className=" title is-italic is-6 is-size-7 has-text-light">{startdate}</p>
+          </div>
+        </div>
+      </div >
+    </div >
+
+
+
+
+
+
+
+  // <div className="column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+  //   <div className="card">
+  //     <div className="card-image">
+  //       <figure className="image is-4by3">
+  //         <img src={largeimageurl}/>
+  //       </figure>
+  //     </div>
+  //     <div className="cardName-content">
+  //       <div className="media">
+  //         <div className="media-left">
+  //         </div>
+  //       </div>
+  //       <div className="content">
+  //         <p>{eventname}</p>
+  //         <br/>
+  //         <time dateTime="2016-1-1">{startdate}</time>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </div>
   
   )
 }
